e820686b349fa019a2d1af600cbc8edb1772bbee
Jayabharathi <Jayabharathi@Jayabharathi-PC>
2020-07-10 14:10:18 +0530

    Square Root program in java

---
+++ src/main/java/Java7102020/SquareRoot.java
@@ -0,0 +1,16 @@
+package Java7102020;
+
+public class SquareRoot {
+	
+	public static void root(int data) {
+		
+		int input=(int) Math.sqrt(data);
+		System.out.println(input);
+		
+	}
+	public static void main(String[] args) {
+		root(36);
+
+	}
+
+}
