fa631451e1a44c0268f49b80a4e41a66ccf6eccc
Jayabharathi <Jayabharathi@Jayabharathi-PC>
2020-08-27 18:12:50 +0530

    Daily Study:Fibonacci and PrimeNo.Thank you

---
+++ src/main/java/Java8272020/Fibonacci.java
@@ -0,0 +1,20 @@
+package Java8272020;
+
+public class Fibonacci {
+
+	public static void main(String[] args) {
+		int n=10;
+		int t1=0;
+		int t2=1;
+		for(int i=0;i<=n;i++) {
+			System.out.println(t1+" + ");
+			int sum=t1+t2;
+			t1=t2;
+			t2=sum;
+			
+			
+		}
+
+	}
+
+}
---
+++ src/main/java/Java8272020/PrimeNumber.java
@@ -0,0 +1,28 @@
+package Java8272020;
+
+public class PrimeNumber {
+	public static void prime(int num)
+	{
+		boolean status=false;
+		for(int i=2;i<=num/2;i++)
+		{
+			if(num%1==0) {
+				status=true;
+				break;
+			}
+			
+			
+		}
+		if(!status) {
+			System.out.println("it is Prime No");
+		}
+		else
+		{
+			System.out.println("it is not Prime No");
+		}
+		
+	}
+	public static void main(String[] args) {
+		prime(39);
+	}
+}
