86ba8079b107bd336bb239fe70d4382ce151de7c
Jayabharathi <Jayabharathi@Jayabharathi-PC>
2020-09-21 21:18:14 +0530

    Daily Practice

--- src/main/java/CodePractiseVersion1/MoveAlltheZeroToEnd.java
+++ src/main/java/CodePractiseVersion1/MoveAlltheZeroToEnd.java
@@ -3,17 +3,17 @@ package CodePractiseVersion1;
 public class MoveAlltheZeroToEnd {
 
 	public static void main(String[] args) {
-		int[] data= {1,0,2,0,3,0,4,0,5,0};
-		int length=data.length;
-		int count=0;
-		for(int i=0;i<length;i++) {
-			if(data[i]!=0)
-			data[count++]=data[i];
+		int[] data = { 1, 0, 2, 0, 3, 0, 4, 0, 5, 0 };
+		int length = data.length;
+		int count = 0;
+		for (int i = 0; i < length; i++) {
+			if (data[i] != 0)
+				data[count++] = data[i];
 		}
-		while(count<length) {
-			data[count++]=0;
+		while (count < length) {
+			data[count++] = 0;
 		}
-		for(int i=0;i<length;i++) {
+		for (int i = 0; i < length; i++) {
 			System.out.println(data[i]);
 		}
 	}
